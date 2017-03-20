c837f9b5c7ae7efa97661b95baf3e0edcdd9e8ab
“Akxsl” <axel.cqn@gmail.com>
2017-03-20 13:05:48 +0100

    gestion du delete en back

--- Admin/index.php
+++ Admin/index.php
@@ -320,6 +320,7 @@ if (!isset($_SESSION['infos_user']['token'])) {
 		<!-- Liens vers mes Scripts -->
 		<script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
 		<script type="text/javascript" src="js/materialize.js"></script>
+            <script type="text/javascript" src="js/api_users.js"></script>
 		<script type="text/javascript" src="js/main.js"></script>
 	</body>
 	</html>
