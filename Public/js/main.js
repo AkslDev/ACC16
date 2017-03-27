0b09d3a82b50a27b4cdea87b599713cfd10e22f6
“Akxsl” <axel.cqn@gmail.com>
2017-03-27 22:59:47 +0200

    Front / Connexion-Inscription-Forget + MAJ

--- Public/js/main.js
+++ Public/js/main.js
@@ -1,6 +1,31 @@
-	// Effet d'agrandissement Boutique
+	// DropDown Barre de Navigation 
+	$('.drop').mouseenter(function(){
+		$( "#drop-connect" ).css("display","block");
+	});
+	$('.drop').mouseleave(function(){
+		$( "#drop-connect" ).css("display","none");
+	});
 
+	// Gestion du Chiffre sur le Bouton Panier dans la Nav
+	// $('btn-panier').on('click', function(){
+
+	// })`
+
+
+
+	// Effet d'agrandissement Boutique
 	$('#btn_prepa, #btn_console, #btn_mob, #btn_acc-int, #btn_susp, #btn_secu, #btn_prot, #btn_toit, #btn_port, #btn_reserv, #btn_sticker, #btn_pickup, #btn_acc-raid, #btn_gamme-or, #btn_gamme-vx, #btn_gamme-fr, #btn_gamme-bm').on('click', function(){
 		$('.shop-left li').removeClass("shop-left-active");
 		$(this).addClass("shop-left-active");
 	});
+
+	// Affichage d'un Produit dans la Boutique
+	$('.shop-card').on('click', function(){
+		$('#list-product').css("display","none");
+		$('#view-product').css("display","block");
+	});
+	// Fermer le produit de la Boutique
+	$('#close-product').on('click', function(){
+		$('#view-product').css("display","none");
+		$('#list-product').css("display","block");
+	})
